1d6533839b2e73ca628c4113ac208691a0c33ab0
terrenceliu01 <terrenceliu01@g.ucla.edu>
2021-06-30 00:51:46 -0500

    homework

---
+++ homework/function/solution/function10.py
@@ -0,0 +1,8 @@
+def trapArea(upper, down, height):
+    upper = input("Enter upper side: ")
+    down = input("Enter down side: ")
+    height = input("Enter height: ")
+    return (upper+down)*height/2
+
+a = trapArea(upper, down, height)
+print()
